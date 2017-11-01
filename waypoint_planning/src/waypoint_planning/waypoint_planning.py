fbaf142b946261d55b396db44fde9c12fd1c98bf
TimWhiting <tim@whitings.org>
2017-10-31 22:07:50 -0600

    Waypoint Planning Gui Updates

--- waypoint_planning/src/waypoint_planning/waypoint_planning.py
+++ waypoint_planning/src/waypoint_planning/waypoint_planning.py
@@ -4,7 +4,12 @@ import rospkg
 
 from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
-from python_qt_binding.QtWidgets import QWidget
+from python_qt_binding.QtWidgets import QWidget, QPushButton, QListWidget, QLineEdit
+from python_qt_binding.QtCore import QRegExp
+from python_qt_binding.QtGui import QRegExpValidator
+from parse import parse
+
+
 
 class WaypointPlanner(Plugin):
 
@@ -32,7 +37,7 @@ class WaypointPlanner(Plugin):
         # Extend the widget with all attributes and children from UI file
         loadUi(ui_file, self._widget)
         # Give QObjects reasonable names
-        self._widget.setObjectName('MyPluginUi')
+        self._widget.setObjectName('Waypoint Planner')
         # Show _widget.windowTitle on left-top of each plugin (when
         # it's set in _widget). This is useful when you open multiple
         # plugins at once. Also if you open multiple instances of your
@@ -42,6 +47,92 @@ class WaypointPlanner(Plugin):
             self._widget.setWindowTitle(self._widget.windowTitle() + (' (%d)' % context.serial_number()))
         # Add widget to the user interface
         context.add_widget(self._widget)
+        # Buttons
+        self.clearWaypointsButton = self._widget.findChild(QPushButton,"clearWaypointsButton")
+        self.clearWaypointsButton.clicked.connect(self.handleClearWaypoints)
+        self.loadFileButton = self._widget.findChild(QPushButton,"loadFileButton")
+        self.loadFileButton.clicked.connect(self.handleLoadFile)
+        self.saveFileButton = self._widget.findChild(QPushButton,"saveFileButton")
+        self.saveFileButton.clicked.connect(self.handleSaveFile)
+        self.addWaypointButton = self._widget.findChild(QPushButton,"addWaypointButton")
+        self.addWaypointButton.clicked.connect(self.handleAddWaypoint)
+        self.deleteWaypointButton = self._widget.findChild(QPushButton,"deleteWaypointButton")
+        self.deleteWaypointButton.clicked.connect(self.handleDeleteWaypoint)
+        self.sendWaypointsButton = self._widget.findChild(QPushButton,"sendWaypointsButton")
+        self.sendWaypointsButton.clicked.connect(self.handleSendWaypoints)
+        #Text Edits
+        self.locationLineEdit = self._widget.findChild(QLineEdit,"locationLineEdit")
+        regexp = QRegExp("\d+,\d+,\d+")
+        validator = QRegExpValidator(regexp)
+        self.locationLineEdit.setValidator(validator)
+        regexp1 = QRegExp("\d+")
+        validator1 = QRegExpValidator(regexp1)
+        self.orientationLineEdit = self._widget.findChild(QLineEdit,"orientationLineEdit")
+        self.orientationLineEdit.setValidator(validator1)
+        self.velocityLineEdit = self._widget.findChild(QLineEdit,"velocityLineEdit")
+        self.velocityLineEdit.setValidator(validator1)
+        #Lists
+        self.newWaypointList = self._widget.findChild(QListWidget,"newWaypointList")
+        self.currentWaypointList = self._widget.findChild(QListWidget,"currentWaypointList")
+        self.waypoints = []
+        self.currentWaypoints = []
+        self.currentWaypoint = None
+        self.newWaypointList.itemClicked.connect(self.newWaypointSelected)
+
+    def waypointFromString(self,text):
+        (x,y,z,o,v) = parse("Waypoint X:{} Y:{} Z:{} {} @degrees, {}kph",text)
+        location = (x,y,z)
+        return (location,o,v)
+
+    def waypointToString(self,waypoint):
+        (location, orientation, velocity) = waypoint
+        (locx, locy, locz) = location
+        return "Waypoint X:{} Y:{} Z:{} @{}degrees, {}kph".format(locx,locy,locz,orientation,velocity)
+
+    def createWaypoint(self,waypoint):
+        self.waypoints.append(waypoint)
+        self.newWaypointList.addItem(self.waypointToString(waypoint))
+
+    def newWaypointSelected(self,item):
+        waypoint = self.waypointFromString(item.text())
+        self.currentWaypoint = waypoint
+
+    def handleClearWaypoints(self):
+        self.clearWaypointsButton.setText("You Cleared It!")
+        pass
+
+    def handleLoadFile(self):
+
+        pass
+
+    def handleSaveFile(self):
+
+        pass
+
+    def handleAddWaypoint(self):
+        text = self.locationLineEdit.text()
+        (x,y,z) = parse('{},{},{}',text)
+        orientation = self.orientationLineEdit.text()
+        velocity = self.velocityLineEdit.text()
+        location = (x,y,z)
+        waypoint = (location, orientation, velocity)
+        self.createWaypoint(waypoint)
+        self.currentWaypoint = waypoint
+        pass
+
+    def handleDeleteWaypoint(self):
+        if self.waypoints is None:
+            print("Can't remove, no more waypoints")
+        self.waypoints.remove(self.currentWaypoint)
+        self.newWaypointList.clear()
+        for waypoint in self.waypoints:
+            self.newWaypointList.addItem(self.waypointToString(waypoint))
+
+
+    def handleSendWaypoints(self):
+
+        pass
+
 
     def shutdown_plugin(self):
         # TODO unregister all publishers here
