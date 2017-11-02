8116ef2c339c1f84985b029ff2db79c8b5f8ff13
TimWhiting <tim@whitings.org>
2017-11-02 16:57:11 -0600

    rosplane plugin added bomb drop and wind estimator, first commit

---
+++ bomb_drop/include/bomb_drop/bomb_drop.h
@@ -0,0 +1,8 @@
+//
+// Created by tim on 11/2/17.
+//
+
+#ifndef PROJECT_BOMB_DROP_H
+#define PROJECT_BOMB_DROP_H
+
+#endif //PROJECT_BOMB_DROP_H
---
+++ wind_estimator/include/wind_estimator/wind_estimator.h
@@ -0,0 +1,8 @@
+//
+// Created by tim on 11/2/17.
+//
+
+#ifndef PROJECT_WIND_ESTIMATOR_H
+#define PROJECT_WIND_ESTIMATOR_H
+
+#endif //PROJECT_WIND_ESTIMATOR_H
