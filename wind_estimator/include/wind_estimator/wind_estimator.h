21516c97896b99a7ce43618b86628d4dadeb3c2e
uavteambyu <uavteambyu@gmail.com>
2017-11-07 17:50:44 -0700

    Began to define functionality for some of the wind estimation functions.

--- wind_estimator/include/wind_estimator/wind_estimator.h
+++ wind_estimator/include/wind_estimator/wind_estimator.h
@@ -12,28 +12,34 @@
 namespace wind_estimator {
     class windEstimator {
     public:
+        windEstimator();
     private:
-        const uint8_t SampleNumber = 50; //number of samples to be used for estimation
-        struct wind {
-            float n[SampleNumber];
-            float e[SampleNumber];
+        const uint8_t MaxSampleNumber = 50; //number of samples to be used for estimation
+        struct wind_array{
+            float wn[MaxSampleNumber];
+            float we[MaxSampleNumber];
+        };
+        struct single_wind_sample{
+            float wn;
+            float we;
         };
         rosplane_plugin_msgs::WindEstimate windEstimate;
         ros::NodeHandle nh_; //public for subscribing, publishing, etc
         ros::NodeHandle nhPrivate_; //private for pulling parameter values from the parameter server
         ros::Subscriber StateSubscriber;
         ros::Publisher WindEstimatePublisher;
-        float StdDeviation;
-        float Mean;
+        single_wind_sample StdDeviation; //holds std_deviation for each direction (treat as independent random variables)
+        single_wind_sample Mean; //holds means value for each direction
         uint8_t ArrayIndex;
-
-
+        uint8_t PresentSampleNumber;
+        wind_array WindSamples;
+        bool ArrayFilled;
 
         void stateCallback(const rosplane_msgs::planeState &msg);
         void publishWindEstimate(); //publish wind
-        //function to calculate std deviation
-
-        //function to calculate confidence
+        void calculateAverage();//function to calculate sample average
+        void calculateStdDev();//function to calculate std deviation
+        float calculateConfidence();// function to calculate confidence
         //
     };
 }
--- wind_estimator/src/wind_estimator.cpp
+++ wind_estimator/src/wind_estimator.cpp
@@ -6,5 +6,80 @@
 #include <cstdlib>
 #include <math.h>
 
+namespace wind_estimator {
+    windEstimator::windEstimator(){
+        ArrayIndex = 0;
+        PresentSampleNumber = 0;
+        ArrayFilled = false;
+        StdDeviation->we = 0;
+        StdDeviation->wn = 0;
+        Mean->we = 0;
+        Mean->wn = 0;
+    }
+    void windEstimator::stateCallback(const rosplane_msgs::planeState &msg){
+        WindSamples.wn[ArrayIndex] = msg.wn;
+        WindSamples.we[ArrayIndex] = msg.we;
+        ROS_INFO("Added wind data, n(%f) e(%f), to sample index %d. We have recorded %d samples.",
+                 WindSamples.wn[ArrayIndex], WindSamples.we[ArrayIndex], ArrayIndex, PresentSampleNumber+1);
+        if(ArrayIndex < 49)
+        {
+            ArrayIndex = ArrayIndex+1;
+        } else
+        {
+            ArrayIndex = 0;
+            ArrayFilled = true;
+            ROS_INFO("Sample array filled. New samples will overwrite old values beginning with the first measurement.");
+        }
+        if(PresentSampleNumber < 50)
+            PresentSampleNumber = PresentSampleNumber+1;
+    }
+    void windEstimator::publishWindEstimate(); //publish wind
+
+    void windEstimator::calculateAverage()//function to calculate sample average
+    {
+        float WnAccumulated = 0;
+        float WeAccumulated = 0;
+
+        for(uint8_t n = 0; n<PresentSampleNumber; n++)
+        {
+            //iterate through the values for wn
+            WnAccumulated += WindSamples.wn[n];
+            //iterate through the values for we
+            WeAccumulated += WindSamples.we[n];
+        }
+        //store in corresponding mean variable after dividing by sample number
+        Mean.wn = WnAccumulated/PresentSampleNumber;
+        Mean.we = WeAccumulated/PresentSampleNumber;
+        ROS_INFO("Average wind value is: n(%f) e(%f)", Mean.wn, Mean.we);
+    }
+    void windEstimator::calculateStdDev()//function to calculate std deviation
+    {
+        windEstimator::calculateAverage();
+        float DifferenceSumWn = 0;
+        float DifferenceSumWe = 0;
+        for(uint8_t n = 0; n<PresentSampleNumber; n++)
+        {
+            DifferenceSumWn += pow((WindSamples.wn[n]-Mean.wn),2);
+            DifferenceSumWe += pow((WindSamples.we[n]-Mean.we),2);
+        }
+        StdDeviation.wn = sqrt((DifferenceSumWn/(PresentSampleNumber-1)));
+        StdDeviation.we = sqrt((DifferenceSumWe/(PresentSampleNumber-1)));
+    }
+    float windEstimator::calculateConfidence()// function to calculate "confidence"
+    {
+        float RatioStdDevMeanWn = 0;
+        float RatioStdDevMeanWe = 0;
+        if(2*StdDeviation.wn/abs(Mean.wn)<=0.1 && 2*StdDeviation.we/abs(Mean.we)<=0.1)
+        {
+            ROS_INFO(" //print out what percentage is within 10% of mean
+            return //whichever is less accurate of the two directions
+        } else
+        {
+            ROS_INFO(" //print out that not x% is within 10% of mean
+            return //whichever is less accurate of the two directions
+        }
+    }
+}
+
 
 
