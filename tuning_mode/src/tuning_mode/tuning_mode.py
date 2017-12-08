1b5b9c1189cd5b2bf485aeb077424d67f9734f82
uavteambyu <uavteambyu@gmail.com>
2017-12-08 10:50:01 -0700

    gui will now run in rqt

--- tuning_mode/src/tuning_mode/tuning_mode.py
+++ tuning_mode/src/tuning_mode/tuning_mode.py
@@ -28,7 +28,7 @@ class TuningMode(Plugin):
         # Create QWidget
         self._widget = QWidget()
         # Get path to UI file which should be in the "resource" folder of this package
-        ui_file = os.path.join(rospkg.RosPack().get_path('tuning_mode'), 'resource', 'tuningMode.ui')
+        ui_file = os.path.join(rospkg.RosPack().get_path('tuning_mode'), 'resource', 'TuningMode.ui')
         # Extend the widget with all attributes and children from UI file
         loadUi(ui_file, self._widget)
         # Give QObjects reasonable names
